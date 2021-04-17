99a916c1a4647cc3c263d6a5af517c7269e40317
Shuvrodeb Kar <shuvrodeb.kar@wundermanthompson.com>
2021-04-17 21:17:01 +0600

    test application add

---
+++ Bookkeeping/ClientApp/src/app/reconciliation/reconciliation.component.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ReconciliationComponent = void 0;
+var ReconciliationComponent = /** @class */ (function () {
+    function ReconciliationComponent() {
+    }
+    return ReconciliationComponent;
+}());
+exports.ReconciliationComponent = ReconciliationComponent;
+//# sourceMappingURL=reconciliation.component.js.map
---
+++ Bookkeeping/ClientApp/src/app/reconciliation/reconciliation.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+//import { BsDatepickerConfig, BsDatepickerViewMode } from 'ngx-bootstrap/datepicker';
+
+@Component({
+  templateUrl: './reconciliation.component.html',
+})
+
+export class ReconciliationComponent implements OnInit  {
+  //datePickerValue: Date = new Date(2020, 7);
+  //dateRangePickerValue: Date[];
+  //range1: Date = new Date(2020, 5);
+  //range2: Date = new Date(2020, 8);
+  //minMode: BsDatepickerViewMode = 'month';
+
+  //bsConfig: Partial<BsDatepickerConfig>;
+
+  ngOnInit(): void {
+    //this.dateRangePickerValue = [this.range1, this.range2];
+    //this.bsConfig = Object.assign({}, {
+    //  minMode: this.minMode
+    //});
+  }
+}
